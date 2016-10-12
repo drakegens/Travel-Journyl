8d3cdbb148d14cf8d037d13fc90268600feb21e0
Drake Gens <drake.gens@gmail.com>
2016-10-12 15:57:07 +0100

    Began the new travel activity layout, adding a couple elements

--- app/src/main/java/drakegens/traveljournyl/NewTravel.java
+++ app/src/main/java/drakegens/traveljournyl/NewTravel.java
@@ -2,12 +2,20 @@ package drakegens.traveljournyl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class NewTravel extends AppCompatActivity {
+
+    TextView header;
+    EditText location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_travel);
+
+        header = (TextView) findViewById(R.id.header);
+        location = (EditText) findViewById(R.id.location);
     }
 }
