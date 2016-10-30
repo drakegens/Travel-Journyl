7918a1834766346917d3f9ed5071b30c022858f2
Drake Gens <drake.gens@gmail.com>
2016-10-30 11:59:55 +0000

    Finished about menu functionality, however i introduced some layout issues with main menu and accidentally made theme too dark

--- app/src/main/java/drakegens/traveljournyl/MainActivity.java
+++ app/src/main/java/drakegens/traveljournyl/MainActivity.java
@@ -1,9 +1,13 @@
 package drakegens.traveljournyl;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     Button existingTravelExperience;
     Button viewTravelFacts;
     Button viewRSSFeed;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +68,44 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.action_button_items, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                //create the about dialog and display it, informing users as to what the application does
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setTitle(R.string.alertDialogAboutTitle);
+                alertDialogBuilder.setMessage(R.string.alertDialogAboutMessage);
+                alertDialogBuilder.setPositiveButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                AlertDialog alert = alertDialogBuilder.create();
+                alert.show();
+                return true;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
 
