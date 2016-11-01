ab10ec7a5d9e37650a438a442220a057cf850fe3
Drake Gens <drake.gens@gmail.com>
2016-11-01 17:46:29 +0000

    Fixed some layout issues, and then added UI components to new travel activity. Worked a bit with DatePicker

--- app/src/main/java/drakegens/traveljournyl/NewTravelActivity.java
+++ app/src/main/java/drakegens/traveljournyl/NewTravelActivity.java
@@ -1,21 +1,70 @@
 package drakegens.traveljournyl;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class NewTravelActivity extends AppCompatActivity {
     //instance variables
-    TextView header;
+    //TextView header;
     EditText location;
+    EditText fromDate;
+    EditText toDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_travel);
 
-        header = (TextView) findViewById(R.id.header);
+        //header = (TextView) findViewById(R.id.header);
         location = (EditText) findViewById(R.id.location);
+        fromDate = (EditText) findViewById(R.id.etFromDate);
+        fromDate.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR);
+                int mMonth = c.get(Calendar.MONTH);
+                int mDay = c.get(Calendar.DAY_OF_MONTH);
+                System.out.println("the selected " + mDay);
+                DatePickerDialog dialog = new DatePickerDialog(NewTravelActivity.this,
+                        new FromDateSetListener(), mYear, mMonth, mDay);
+                dialog.show();
+            }
+
+        });
     }
+
+
+    class FromDateSetListener implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            // getCalender();
+//            int mYear = year;
+//            int mMonth = monthOfYear;
+//            int mDay = dayOfMonth;
+            fromDate.setText(new StringBuilder()
+                    // Month is 0 based so add 1
+                    .append(monthOfYear + 1).append("/").append(dayOfMonth).append("/")
+                    .append(year).append(" "));
+            //System.out.println(v.getText().toString());
+
+
+        }
+    }
+
+
+
+
 }
+
